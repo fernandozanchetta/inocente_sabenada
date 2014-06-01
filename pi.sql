0ec238ed3a944f501a3664ecfc1dba1051dab06d
Diego Moreira <diegorm.ti@gmail.com>
2014-06-01 14:53:27 -0300

    banco

--- pi.sql
+++ pi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 12-Maio-2014 às 01:26
+-- Generation Time: 01-Jun-2014 às 19:52
 -- Versão do servidor: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -30,6 +30,20 @@ CREATE TABLE IF NOT EXISTS `materias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `materia` varchar(100) NOT NULL,
   `descricao` varchar(255) NOT NULL,
+  `clicks` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `materias_usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `materias_usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `materia_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -41,9 +55,10 @@ CREATE TABLE IF NOT EXISTS `materias` (
 
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `conteudo` text NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
   `downloads` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -60,16 +75,18 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `usuario` varchar(50) NOT NULL,
   `email` varchar(200) NOT NULL,
   `senha` text NOT NULL,
+  `status` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `usuario`, `email`, `senha`) VALUES
-(2, 'teste1', 'teste2', 'teste1@teste.com.br', 'e10adc3949ba59abbe56e057f20f883e'),
-(3, 'Diego Moreira', 'dinego', 'diegorm.ti@gmail.com', 'aa1bf4646de67fd9086cf6c79007026c');
+INSERT INTO `usuarios` (`id`, `nome`, `usuario`, `email`, `senha`, `status`) VALUES
+(2, 'teste1', 'teste2', 'teste1@teste.com.br', 'e10adc3949ba59abbe56e057f20f883e', 0),
+(3, 'Diego Moreira', 'dinego', 'diegorm.ti@gmail.com', 'aa1bf4646de67fd9086cf6c79007026c', 0),
+(4, 'teste', 'generico2', 'generico2', '6f434d8e0317b82c2c25679e0a0e6678', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
